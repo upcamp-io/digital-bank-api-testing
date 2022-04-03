699b1eda7d866e0dc4da6da1f1c8e6cffa42660d
Santiago Morelle <96072727+santiago-abstracta@users.noreply.github.com>
2022-04-03 13:49:36 -0300

    Update axios.config.js

--- axios.config.js
+++ axios.config.js
@@ -1,15 +1,21 @@
-import axios from 'axios';
-import config from './config.js';
+import axios from "axios";
+import config from "./config.js";
 
-let response = await axios.post(`${config.baseUrl}/auth`,{},{
+let response = await axios.post(
+  `${config.baseUrl}/auth`,
+  {},
+  {
     params: {
-        username: config.adminUsername,
-        password: config.adminPassword
-    }
-});
-const instance = axios.create({
-    baseURL: config.baseUrl,
-    headers: {'Authorization': `Bearer ${response.data.authToken}`}
-  });
+      username: config.adminUsername,
+      password: config.adminPassword,
+    },
+  }
+);
 
-  export default instance
+const instance = axios.create({
+  validateStatus: () => true,
+  baseURL: config.baseUrl,
+  headers: { Authorization: `Bearer ${response.data.authToken}` },
+});
+
+export default instance;
